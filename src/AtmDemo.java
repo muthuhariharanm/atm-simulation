6e5fb5adad144276e50a76db36160c0aee8c8654
muthuhariharanm <muthuhariharanm@gmail.com>
2019-12-23 20:21:40 +0530

    Project done

---
+++ src/AtmDemo.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Muthu Hariharan M on 9/27/2018.
+ */
+package oopsProject;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.lang.*;
+
+public class AtmDemo
+{
+  //  public static int pin = 1000;
+    public static void main(String []args)
+    {
+        LoginPage loginPage = new LoginPage();
+    }
+}
---
+++ src/UserDetails.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Muthu Hariharan M on 10/17/2018.
+ */
+package oopsProject;
+
+import java.io.*;
+
+public class UserDetails implements Serializable
+{
+    public String name, userName, mailId, amount, password;
+
+    public UserDetails(String a,String b, String c,String d,String e)
+    {
+        name = a;
+        userName = b;
+        mailId = c;
+        amount = d;
+        password = e;
+    }
+    public String retName()
+    {
+        return name;
+    }
+    public String retUserName()
+    {
+        return userName;
+    }
+    public String SretMailId()
+    {
+        return mailId;
+    }
+    public String retAmount()
+    {
+        return amount;
+    }
+    public String retPassword()
+    {
+        return password;
+    }
+}
